2b1a5705720c6a066fc2019b4ceb20ebb598fd92
王翔 <wangjian@baofoo.com>
2018-11-26 14:43:48 +0800

    init

---
+++ src/main/java/com/lionxxw/controller/GirlController.java
@@ -0,0 +1,105 @@
+package com.lionxxw.controller;
+
+import com.lionxxw.bean.Girl;
+import com.lionxxw.dao.GirlRepository;
+import com.lionxxw.domain.Result;
+import com.lionxxw.service.GirlService;
+import com.lionxxw.utils.ResultUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * Package com.lionxxw.controller
+ * Project girl
+ *
+ * Author lionxxw
+ * Created on 2017/2/27 19:31
+ * version 1.0.0
+ */
+@RestController
+public class GirlController {
+    @Autowired
+    private GirlRepository girlRepository;
+    @Autowired
+    private GirlService girlService;
+
+    /**
+     * 查询所有女生列表
+     * @return
+     */
+    @GetMapping(value = "/girls")
+    public Result<List<Girl>> girlList(){
+        return ResultUtils.getSuccess(girlRepository.findAll());
+    }
+
+    /**
+     * 新增一个女生
+     * @return
+     */
+    @PostMapping(value = "/girls")
+    public Result<Girl> girlAdd(@Valid Girl girl, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return ResultUtils.getFail(-1, bindingResult.getFieldError().getDefaultMessage());
+        }
+        return ResultUtils.getSuccess(girlRepository.save(girl));
+    }
+
+    /**
+     * 查询一个女生
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/girls/{id}")
+    public Result<Girl> girlFindOne(@PathVariable("id") Long id){
+        return ResultUtils.getSuccess(girlRepository.findOne(id));
+    }
+
+    /**
+     * 更新女生信息
+     * @param id
+     * @param girl
+     * @return
+     */
+    @PostMapping(value = "/girls/{id}")
+    public Result<Girl> girlUpdate(@PathVariable("id") Long id, Girl girl){
+        girl.setId(id);
+        return ResultUtils.getSuccess(girlRepository.save(girl));
+    }
+
+    /**
+     * 删除一个女生
+     * @param id
+     */
+    @DeleteMapping(value = "/girls/{id}")
+    public void girlDelete(@PathVariable("id") Long id){
+        girlRepository.delete(id);
+    }
+
+    /**
+     * 根据年龄查询女生列表
+     * @param age
+     * @return
+     */
+    @GetMapping(value = "/girls/age/{age}")
+    public Result<List<Girl>> girlFindByAge(@PathVariable("age") Integer age){
+        return ResultUtils.getSuccess(girlRepository.findByAge(age));
+    }
+
+    /**
+     * 插入2条数据,模拟事务回滚
+     */
+    @GetMapping(value = "/girls/two")
+    public void girlInsertTwo(){
+        girlService.insertTwo();
+    }
+
+    @GetMapping(value = "/girls/getAge/{id}")
+    public Result<Girl> getGirlAge(@PathVariable("id") Long id) throws Exception{
+        Girl girl = girlService.getGirlAge(id);
+        return ResultUtils.getSuccess(girl);
+    }
+}
---
+++ src/main/java/com/lionxxw/service/GirlService.java
@@ -0,0 +1,55 @@
+package com.lionxxw.service;
+
+import com.lionxxw.bean.Girl;
+import com.lionxxw.dao.GirlRepository;
+import com.lionxxw.enums.ResultEnum;
+import com.lionxxw.exception.GirlException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Package com.lionxxw.service
+ * Project girl
+ *
+ * Author lionxxw
+ * Created on 2017/2/27 20:07
+ * version 1.0.0
+ */
+@Service
+public class GirlService {
+    @Autowired
+    private GirlRepository girlRepository;
+
+    @Transactional
+    public void insertTwo(){
+        Girl a = new Girl();
+        a.setAge(18);
+        a.setCupSize("A");
+        girlRepository.save(a);
+
+        Girl b = new Girl();
+        b.setCupSize("G");
+        b.setAge(30);
+        int i = 1/0;/*模拟异常*/
+        girlRepository.save(b);
+    }
+
+    public Girl getGirlAge(Long id) throws Exception {
+        Girl girl = girlRepository.findOne(id);
+        if (girl == null){
+            throw new GirlException(ResultEnum.NULL);
+        }
+        Integer age = girl.getAge();
+        if (null == age){
+            throw new GirlException(ResultEnum.ARGS_IS_NULL);
+        }
+        if (age < 12){
+            throw new GirlException(ResultEnum.PRIMARY_SCHOOL);
+        }
+        if (age < 18){
+            throw new GirlException(ResultEnum.MIDDLE_SCHOOL);
+        }
+        return girl;
+    }
+}
